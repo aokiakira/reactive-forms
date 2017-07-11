5a32491c793be740c0153da4def6fb548694ea16
Hans <aokiakira@users.noreply.github.com>
2017-07-11 23:31:08 +0800

    Add more FormControl to the component

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,12 +1,15 @@
 import {Component} from '@angular/core';
 import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 
+import {states} from './data-model';
+
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html'
 })
 export class HeroDetailComponent {
   heroForm: FormGroup;
+  states = states;
 
   constructor(private formBuilder: FormBuilder) {
     this.createForm();
@@ -14,7 +17,13 @@ export class HeroDetailComponent {
 
   createForm() {
     this.heroForm = this.formBuilder.group({
-      name: ['', Validators.required]
+      name: ['', Validators.required],
+      street: '',
+      city: '',
+      state: '',
+      zip: '',
+      power: '',
+      sidekick: ''
     });
   }
 }
