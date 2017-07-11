6e40c973ee361cb0db8369558a2f6e9d8282d437
Hans <aokiakira@users.noreply.github.com>
2017-07-11 22:58:14 +0800

    Use FormBuilder

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,12 +1,20 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html'
 })
 export class HeroDetailComponent {
-  heroForm = new FormGroup({
-    name: new FormControl()
-  });
+  heroForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.heroForm = this.formBuilder.group({
+      name: ''
+    });
+  }
 }
