7922a0c0a5bd00fdf1524eea59344267d5af8d19
Hans <aokiakira@users.noreply.github.com>
2017-07-11 22:38:35 +0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {ReactiveFormsModule} from '@angular/forms';
+
+import {AppComponent} from './app.component';
+import {HeroDetailComponent} from './hero-detail.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
